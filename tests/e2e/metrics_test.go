e3c0bb2b656c4feaa542ed48dca3ee40ba0bd91f
Gladkov Alexey <agladkov@redhat.com>
2019-02-19 15:26:07 +0100

    Allow to use own certificate for metrics endpoint
    Signed-off-by: Gladkov Alexey <agladkov@redhat.com>

--- tests/e2e/metrics_test.go
+++ tests/e2e/metrics_test.go
@@ -36,6 +36,10 @@ func TestV3MetricsInsecure(t *testing.T) {
 	testCtl(t, metricsTest)
 }
 
+func TestV3MetricsSecureTLSCertAuth(t *testing.T) {
+	testCtl(t, metricsTestCertAuth, withCfg(configMetricsTLS))
+}
+
 func metricsTest(cx ctlCtx) {
 	if err := ctlV3Put(cx, "k", "v", ""); err != nil {
 		cx.t.Fatal(err)
@@ -54,6 +58,47 @@ func metricsTest(cx ctlCtx) {
 		cx.t.Fatalf("failed get with curl (%v)", err)
 	}
 	if err := cURLGet(cx.epc, cURLReq{endpoint: "/health", expected: `{"health":"true"}`, metricsURLScheme: cx.cfg.metricsURLScheme}); err != nil {
+		cx.t.Fatalf("failed get with curl (%v)", err)
+	}
+}
+
+func metricsTestCertAuth(cx ctlCtx) {
+	//	fmt.Printf("%#+v\n", cx.epc.procs[0].Config())
+	//	fmt.Printf("\n")
+	//	return
+
+	if err := ctlV3Put(cx, "k", "v", ""); err != nil {
+		cx.t.Fatal(err)
+	}
+	if err := cURLGet(cx.epc, cURLReq{endpoint: "/metrics", expected: `etcd_debugging_mvcc_keys_total 1`, metricsURLScheme: cx.cfg.metricsURLScheme, useCertAuth: true}); err != nil {
+		cx.t.Fatalf("failed get with curl (%v)", err)
+	}
+	if err := cURLGet(cx.epc, cURLReq{endpoint: "/metrics", expected: fmt.Sprintf(`etcd_server_version{server_version="%s"} 1`, version.Version), metricsURLScheme: cx.cfg.metricsURLScheme, useCertAuth: true}); err != nil {
+		cx.t.Fatalf("failed get with curl (%v)", err)
+	}
+	ver := version.Version
+	if strings.HasSuffix(ver, "+git") {
+		ver = strings.Replace(ver, "+git", "", 1)
+	}
+	if err := cURLGet(cx.epc, cURLReq{endpoint: "/metrics", expected: fmt.Sprintf(`etcd_cluster_version{cluster_version="%s"} 1`, ver), metricsURLScheme: cx.cfg.metricsURLScheme, useCertAuth: true}); err != nil {
+		cx.t.Fatalf("failed get with curl (%v)", err)
+	}
+	if err := cURLGet(cx.epc, cURLReq{endpoint: "/health", expected: `{"health":"true"}`, metricsURLScheme: cx.cfg.metricsURLScheme, useCertAuth: true}); err != nil {
+		cx.t.Fatalf("failed get with curl (%v)", err)
+	}
+
+	req := cURLReq{endpoint: "/metrics", metricsURLScheme: cx.cfg.metricsURLScheme}
+
+	expectErr := []string{
+		"curl: (60) SSL certificate problem: unable to get local issuer certificate",
+		"More details here: https://curl.haxx.se/docs/sslcerts.html",
+		"",
+		"curl failed to verify the legitimacy of the server and therefore could not",
+		"establish a secure connection to it. To learn more about this situation and",
+		"how to fix it, please visit the web page mentioned above.",
+	}
+
+	if err := spawnWithExpects(cURLPrefixArgs(cx.epc, "GET", req), expectErr...); err != nil {
 		cx.t.Fatalf("failed get with curl (%v)", err)
 	}
 }
